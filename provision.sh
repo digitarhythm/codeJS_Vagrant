e7883634c532f5c62e7050bfdf0e47a4d0dd448d
Kow Sakazaki <kow@prominence.tv>
2013-10-24 03:24:20 +0900

    first commit

---
+++ provision.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get -y install apache2
+sudo apt-get -y install php5-cli
+sudo apt-get -y install php5-cgi
+sudo apt-get -y install smarty
+sudo apt-get -y install sqlite3
+sudo apt-get -y install coffeescript
+sudo apt-get -y install imagemagick
